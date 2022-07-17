cddf00a2290ba21ac9f3ab196e2c1f64a9367249
Szymon Forst <szymonforstbiznes@gmail.com>
2022-07-17 18:52:01 +0200

    Added useState to From amount and currency

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
+
 import './App.css';
 import Form from './Form';
 import Result from './Result';
 
+
+
 function App() {
- 
+
+  const CalculateResult = (amount, currency) => {
+    const rateEUR = 4.5817;
+    const rateCHF = 4.3392;
+    const rateSEK = 0.4382;
+
+    switch (currency) {
+      case "EUR":
+        return amount / rateEUR;
+
+      case "CHF":
+        return amount / rateCHF;
+
+      case "SEK":
+        return amount / rateSEK;
+
+        default:
+    };
+  };
+
+
   return (
   <>
   <div className="container">
     <h1 className="header">Kalkulator walutowy</h1>
-    <Form>
-
-    </Form>
-    <Result>
-      
-    </Result>
+    <Form
+    title="Podaj Kwotę:"
+    subTitle="Wybierz walutę:"
+    calculateResult={CalculateResult}
+    />
+    <Result
+    />
   </div>
   </>
   );
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,20 +1,42 @@
 import "./style.css";
+import { useState } from 'react';
+const Form = (props) => {
 
-const Form = () => {
-  return(
-  <form className="form">
+ 
+  const [amount,setAmount] = useState("")
+  const [currency,setCurrency] = useState("EUR")
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    props.calculateResult(amount, currency)
+    console.log(amount);
+    console.log(currency);
+    console.log(props.calculateResult(amount, currency));
+  }
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
       <fieldset className="form__fieldset">
         <div className="form__inputBox">
-            <span className="form__Text">Podaj kwotę:</span>
-            <input className="form__field js-amount" name="amount" type="number" min="1" step="any"  max="999999999" placeholder="Wpisz w kwotę w zł" required />
+          <span className="form__Text">{props.title}</span>
+          <input className="form__field js-amount"
+            value={amount}
+            onChange={(event) => {setAmount(event.target.value)}}
+            type="number"
+            min="1"
+            step="any"
+            max="100000000"
+            placeholder="Wpisz w kwotę w zł"
+            required />
         </div>
         <div>
-            <span className="form__Text">Wybierz walute:</span>
-            <select className=" form__currency" name="chooseCurrency">
-              <option value="EUR" className="form__field ">Euro - EUR</option>
-              <option value="SEK" className="form__field ">Korona Szwedzka - SEK</option>
-              <option value="CHF" className="form__field ">Frank Szwajcarski - CHF</option>
-            </select>
+          <span className="form__Text">{props.subTitle}</span>
+          <select
+            className="form__currency"
+            onChange={(event) => {setCurrency(event.target.value)}}
+            name="chooseCurrency">
+            <option value="EUR" className="form__field ">Euro - EUR</option>
+            <option value="SEK" className="form__field ">Korona Szwedzka - SEK</option>
+            <option value="CHF" className="form__field ">Frank Szwajcarski - CHF</option>
+          </select>
         </div>
       </fieldset>
       <p>
