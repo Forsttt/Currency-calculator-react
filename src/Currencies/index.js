ef733aa7faa94028f53b18a3401118423c608134
Szymon Forst <szymonforstbiznes@gmail.com>
2022-08-09 20:27:14 +0200

    added Currencies

---
+++ src/Currencies/index.js
@@ -0,0 +1,19 @@
+
+ export const Currencies = [
+  {
+    name: "Euro",
+    shortcut: "EUR",
+    rate: 4.74
+  },
+  {
+    name: "Dolar",
+    shortcut: "USD",
+    rate: 4.63
+  },
+  {
+    name: "Frank Szwajcarski",
+    shortcut: "CHF",
+    rate: 4.87
+  },
+];
+
